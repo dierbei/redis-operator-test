52dbe7b81eb01e458f7f7d52729bbd1a933e1494
xiaolatiao <1628652790@qq.com>
2022-10-14 21:07:17 +0800

    feat: 完成重建Pod、清理资源

---
+++ controllers/redis_controller.go
@@ -0,0 +1,108 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	stdlog "log"
+	"redis-demo/service"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	redisv1 "redis-demo/api/v1"
+)
+
+// RedisReconciler reconciles a Redis object
+type RedisReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=redis.hedui.com,resources=redis,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=redis.hedui.com,resources=redis/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=redis.hedui.com,resources=redis/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Redis object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+func (r *RedisReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	var (
+		isUpdate = false
+	)
+
+	obj := &redisv1.Redis{}
+	if err := r.Get(context.Background(), req.NamespacedName, obj); err != nil {
+		stdlog.Println("父资源不存在，重建资源失败, err:", err.Error())
+		return ctrl.Result{}, nil
+	}
+
+	if !obj.DeletionTimestamp.IsZero() {
+		return ctrl.Result{}, service.Delete(r.Client, obj)
+	}
+
+	podNameList := service.GenPodName(obj.Name, obj.Spec.Replicas)
+	for _, name := range podNameList {
+		podNameRes, err := service.CreatePod(r.Client, obj, name, r.Scheme)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		if podNameRes != nil {
+			if controllerutil.ContainsFinalizer(obj, *podNameRes) {
+				continue
+			}
+
+			obj.Finalizers = append(obj.Finalizers, *podNameRes)
+			isUpdate = true
+		}
+	}
+
+	// 更新
+	if isUpdate {
+		stdlog.Println("检测到资源更新:", obj.Finalizers)
+		if err := r.Update(context.Background(), obj); err != nil {
+			stdlog.Println(err)
+			return ctrl.Result{}, err
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *RedisReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&redisv1.Redis{}).
+		Watches(
+			&source.Kind{Type: &v1.Pod{}},
+			handler.Funcs{
+				DeleteFunc: service.RebuildPod,
+			},
+		).
+		Complete(r)
+}
---
+++ service/service.go
@@ -0,0 +1,114 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	stdlog "log"
+
+	v1 "k8s.io/api/core/v1"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/workqueue"
+	redisv1 "redis-demo/api/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+func CreatePod(client client.Client, obj *redisv1.Redis, podName string, schema *runtime.Scheme) (*string, error) {
+	if PodIsNotAlreadyExist(client, obj.Namespace, podName) {
+		return nil, nil
+	}
+
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      podName,
+			Namespace: obj.Namespace,
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:            podName,
+					Image:           "redis:5-alpine",
+					ImagePullPolicy: v1.PullIfNotPresent,
+					Ports: []v1.ContainerPort{
+						{
+							ContainerPort: int32(obj.Spec.Port),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// set ownerReferences
+	if err := controllerutil.SetControllerReference(obj, pod, schema); err != nil {
+		return nil, err
+	}
+
+	if err := client.Create(context.Background(), pod); err != nil {
+		return nil, err
+	}
+	stdlog.Println("创建新的Pod资源:", pod.Name)
+
+	return &podName, nil
+}
+
+// GenPodName 生成 Pod Name
+// example: ["myhedui-0", "myhedui-1" ... ]
+func GenPodName(prefix string, replicas int) []string {
+	ans := make([]string, 0)
+	for i := 0; i < replicas; i++ {
+		ans = append(ans, fmt.Sprintf("%s-%d", prefix, i))
+	}
+	return ans
+}
+
+// RebuildPod 重建被删除的 Pod
+func RebuildPod(evt event.DeleteEvent, ltf workqueue.RateLimitingInterface) {
+	stdlog.Println("检测到有Pod删除，即将重建:", evt.Object.GetName())
+
+	for _, ref := range evt.Object.GetOwnerReferences() {
+		if ref.Kind == "Redis" && ref.APIVersion == "redis.hedui.com/v1" {
+			ltf.Add(reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      ref.Name,
+					Namespace: evt.Object.GetNamespace(),
+				},
+			})
+		}
+	}
+}
+
+// PodIsNotAlreadyExist 检测 Pod 是否已经创建
+func PodIsNotAlreadyExist(client client.Client, podNamespace, podName string) bool {
+	if err := client.Get(context.Background(), types.NamespacedName{
+		Namespace: podNamespace,
+		Name:      podName,
+	}, &v1.Pod{}); err != nil {
+		return false
+	}
+
+	return true
+}
+
+func Delete(client client.Client, obj *redisv1.Redis) error {
+	stdlog.Println("检测到删除资源，正在清理Pod、Finalizers...")
+	for _, name := range obj.Finalizers {
+		if err := client.Delete(context.Background(), &v1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: obj.Namespace,
+			},
+		}); err != nil {
+			return err
+		}
+	}
+
+	obj.Finalizers = []string{}
+	stdlog.Println("检测到删除资源，资源清理完毕...")
+	return client.Update(context.Background(), obj)
+}
